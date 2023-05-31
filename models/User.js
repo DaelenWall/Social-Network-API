0398023cadb792644908dfe5b3ce95f994d84a61
Daelen Wall <daelenwall1@gmail.com>
2023-05-31 17:33:38 -0400

    index model + User model

---
+++ models/User.js
@@ -0,0 +1,50 @@
+const { Schema, Types } = require('mongoose');
+
+// Schema to create new user
+const userSchema = new Schema(
+  {
+    id: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+
+});
+
+const User = model('User', userSchema)
+
+module.exports = User;
