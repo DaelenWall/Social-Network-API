258d489f579eab6d98e5ef27fd3ee5b44343daca
Daelen Wall <daelenwall1@gmail.com>
2023-06-01 00:39:14 -0400

    routing thoughtController methods in thoughtRoutes.js + routing reactions

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,1 +1,28 @@
+const router = require('express').Router();
+const {
+  getAllThoughts,
+  getSingleThought,
+  createThought,
+  updateThought,
+  deleteThought,
+  addThoughtReaction,
+  removeThoughtReaction,
+} = require('../../controllers/thoughtController');
 
+// Route:  /api/thoughts
+router.route('/').get(getAllThoughts).post(createThought);
+
+// Route:  /api/thoughts/:thought_id
+router
+  .route('/:thought_id')
+  .get(getSingleThought)
+  .put(updateThought)
+  .delete(deleteThought);
+
+// Route:  /api/thoughts/:thought_id/reactions
+router.route('/:thought_id/reactions').post(addThoughtReaction);
+
+// Route:  /api/videos/:thought_id/reaction/:reaction_id
+router.route('/:thought_id/reactions/:reaction_id').delete(removeThoughtReaction);
+
+module.exports = router;
